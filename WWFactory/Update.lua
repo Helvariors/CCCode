5847ece7684bfbab08ebaab08c390bdce0e022a9
HWSxDejv <davedinox@gmail.com>
2025-04-10 23:36:02 +0200

    fixed the github link

--- WWFactory/Update.lua
+++ WWFactory/Update.lua
@@ -8,9 +8,9 @@ shell.run("delete", "Util")
 os.sleep(.4)
 
 
-shell.run("wget", "https://raw.githubusercontent.com/Helvariors/CCCode/WWFactory/Update.lua", "Update")
+shell.run("wget", "https://raw.githubusercontent.com/Helvariors/CCCode/main/WWFactory/Update.lua", "Update")
 os.sleep(.4)
-shell.run("wget", "https://raw.githubusercontent.com/Helvariors/CCCode/WWFactory/Util.lua", "Util")
+shell.run("wget", "https://raw.githubusercontent.com/Helvariors/CCCode/main/WWFactory/Util.lua", "Util")
 os.sleep(.4)
 
 
